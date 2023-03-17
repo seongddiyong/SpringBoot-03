537b26f960af3248f46c18597dbee88ed0c25967
seongddiyong <kodd1102@naver.com>
2023-03-18 01:37:45 +0900

    first commit

---
+++ src/main/java/hello/springpark/SpringParkApplication.java
@@ -0,0 +1,13 @@
+package hello.springpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringParkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/springpark/SpringParkApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springpark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
